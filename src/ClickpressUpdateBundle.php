47805e1c413e40cdbe9062db6e1f1851a9176e47
stefansl <ssl@clickpress.de>
2022-10-25 18:50:36 +0200

    Remove docblock

--- src/ClickpressUpdateBundle.php
+++ src/ClickpressUpdateBundle.php
@@ -2,14 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the metten-promocodes-bundle.
- *
- * (c) Stefan Schulz-Lauterbach
- *
- * @license proprietary
- */
-
 namespace Clickpress\Update;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
