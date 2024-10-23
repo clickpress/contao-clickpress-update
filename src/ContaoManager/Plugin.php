561f3fa900037e52655232d156b8ef78a3e037e6
stefansl <ssl@clickpress.de>
2024-10-23 15:06:09 +0200

    Refactor routing configuration
    Removed routes.yaml and updated routing logic in Plugin.php to load routes directly from the controller directory. Additionally, modified the route annotation in FrontendApiController for improved clarity.

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -23,8 +23,9 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface
 
     public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
     {
-        $file = __DIR__.'/../../config/routes.yaml';
-
-        return $resolver->resolve($file)->load($file);
+        return $resolver
+            ->resolve(__DIR__.'/../Controller')
+            ->load(__DIR__.'/../Controller')
+        ;
     }
 }
